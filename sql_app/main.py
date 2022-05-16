08278003eb361e89e7df18df90be39701b30535b
hugo.labedade@ynov.com <hugo.labedade@ynov.com>
2022-05-16 21:16:41 +0200

    transfert dl csv

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,3 +1,5 @@
+from os import path
+import requests
 from io import BytesIO
 from typing import Any
 from fastapi import FastAPI, Request, Depends, Response
@@ -35,6 +37,21 @@ def get_db():
 
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+csv_dataset = pd.read_csv("./csv_dataset.csv", delimiter=";", on_bad_lines='skip',
+                          usecols=["dataset.title", "dataset.url", "url", "title"])
+
+lien1 = "https://www.data.gouv.fr/fr/datasets/r/b09da629-e48a-4baa-8d43-9ae28b3248da"
+lien2 = "https://www.data.gouv.fr/fr/datasets/r/f4d7c0bd-8dc7-49af-8333-ec9d6abfdbee"
+csv1 = "vacsi-pc-a_fra.csv"
+csv2 = "covid-cedc-quot.csv"
+
+if path.exists('vacsi-pc-a_fra.csv') == False & path.exists('covid-cedc-quot.csv') == False:
+    response = requests.get(lien1)
+    csv_file = open(f'./csv/{csv1}', "wb").write(response.content)
+
+    response = requests.get(lien2)
+    csv_file2 = open(f'./csv/{csv2}', "wb").write(response.content)
 
 
 csv_dataset = pd.read_csv("./csv_dataset.csv", delimiter=";",
