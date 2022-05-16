e0873faa97cfa1b0672b2aed3435370190bdeeb3
hugo.labedade@ynov.com <hugo.labedade@ynov.com>
2022-05-16 18:28:45 +0200

    dernier correctif sur plot

--- sql_app/main.py
+++ sql_app/main.py
@@ -37,19 +37,19 @@ def get_db():
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-
-csv_dataset = pd.read_csv("./csv_dataset.csv", delimiter=";", on_bad_lines='skip', usecols=["dataset.title", "dataset.url", "url"])
+csv_dataset = pd.read_csv("./csv_dataset.csv", delimiter=";",
+                          on_bad_lines='skip', usecols=["dataset.title", "dataset.url", "url"])
 
 
 data_covid1 = pd.read_csv("./csv/covid-cedc-quot.csv", delimiter=";",
-                        usecols=[
-                            "clage_vacsi", 
-                            "jour", 
-                            "pc_vac_dose1", 
-                            "pc_vac_dose2", 
-                            "pc_vac_cum_dose1", 
-                            "pc_vac_cum_dose2"
-                        ])
+                          usecols=[
+                              "clage_vacsi",
+                              "jour",
+                              "pc_vac_dose1",
+                              "pc_vac_dose2",
+                              "pc_vac_cum_dose1",
+                              "pc_vac_cum_dose2"
+                          ])
 
 data_covid1 = data_covid1.rename(columns={"clage_vacsi": "classe_age", "pc_vac_dose1": "premiere_dose",
                                  "pc_vac_dose2": "deuxieme_dose", "pc_vac_cum_dose1": "premiere_dose_cum", "pc_vac_cum_dose2": "deuxieme_dose_cum"})
@@ -57,18 +57,28 @@ data_covid1 = data_covid1.rename(columns={"clage_vacsi": "classe_age", "pc_vac_d
 data_covid1.to_sql('Morbid', con=engine, if_exists='replace')
 engine.execute("SELECT * FROM Morbid").fetchall()
 
-def en_json():
+data_covid2 = pd.read_csv("./csv/vacsi-pc-a_fra.csv", delimiter=";",
+                          usecols=[
+                              "reg",
+                              "cl_age90",
+                              "Dc_Elec_Covid_cum",
+                              "jour"
+                          ])
+
+data_covid2 = data_covid2.rename(columns={"reg": "region", "cl_age90": "classe_age", "Dc_Elec_Covid_cum": "deces_covid"})
+data_covid2.to_sql('Deces', con=engine, if_exists='replace')
+
+
+def table1():
     df = pd.read_sql_query("SELECT * FROM Morbid", con=engine).to_json()
     parsed = json.loads(df)
     return parsed
 
 
-
-
 @app.route('/', methods=("POST", "GET"))
 async def index(request: Request):
     ekip = "ekip"
-    return templates.TemplateResponse('index.html',{"request": request, "ekip": ekip} )
+    return templates.TemplateResponse('index.html', {"request": request, "ekip": ekip})
 
 
 @app.get("/morbid/", response_model=List[schemas.Morbid])
@@ -81,7 +91,7 @@ def read_morbid(skip: int = 0, db: Session = Depends(get_db)):
 async def index(request: Request):
     df = pd.read_sql_query("SELECT * FROM Morbid", con=engine)
     return Response(df.to_json(orient="records"), media_type="application/json")
-    #return HTMLResponse(content=df.to_html(), status_code=200)
+    # return HTMLResponse(content=df.to_html(), status_code=200)
 
 
 @app.route('/graph', methods=("POST", "GET"))
@@ -97,5 +107,35 @@ async def graph(request: Request):
     plt.savefig(buf, format="png")
     buf.seek(0)
     return StreamingResponse(buf, media_type="image/png")
-    
 
+
+@app.route('/graph2', methods=("POST", "GET"))
+async def graph2(requesti: Request):
+    df2 = pd.read_sql_query("SELECT * FROM Deces", con=engine)
+    plt.figure(figsize=(12, 12))
+    plt.scatter(df2.region, df2.deces_covid, c="blue", marker=".")
+    plt.xticks(df2.region)
+    plt.xlabel("Régions")
+    plt.ylabel("Nombre de décès")
+    plt.title("Nombre de décès en fonction de la région")
+    plt.plot
+    buf = BytesIO()
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
+
+
+@app.route('/graph3', methods=("POST", "GET"))
+async def graph3(requesti: Request):
+    df3 = pd.read_sql_query("SELECT * FROM Deces", con=engine)
+    plt.figure(figsize=(10, 10))
+    plt.scatter(df3.jour, df3.deces_covid, c="blue", marker=".")
+    plt.xticks(np.arange(0, 100, 10.0))
+    plt.xlabel("Date")
+    plt.ylabel("Nombre de décès")
+    plt.title("Nombre de décès dans le temps et par région")
+    plt.plot
+    buf = BytesIO()
+    plt.savefig(buf, format="png")
+    buf.seek(0)
+    return StreamingResponse(buf, media_type="image/png")
